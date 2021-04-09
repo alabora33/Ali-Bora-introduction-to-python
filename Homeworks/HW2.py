ecdab45d5532d68aa53535945596e297c8d3d899
alabora33 <41023507+alabora33@users.noreply.github.com>
2021-04-09 14:02:49 +0300

    Update HW2.py

--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -1,5 +1,57 @@
-#Explain your work
+"""
+kullanıcı giriş uygulaması:
+-kullanıcıdan kullanıcı adı ve şifre değerlerini alın.
+-if ifadesindeki değerleri kontrol edin ve başarılı olup olmadıklarını söyleyin
+ekstra:
+-Aynı kullanım giriş uygulamasını oluşturmaya çalışın, ancak bu sefer sözlüğü kullanın
+"""
 
-#Question 1
-for x in range(a):
-	print(a)
+###user login application####
+
+username = "alabora33"
+password = "12345"
+
+kullaniciadi = input("Kullanıcı adını giriniz:")
+sifre = input("Şifreyi giriniz:")
+
+if (kullaniciadi==username and sifre==password):
+    print("Başarıyla giriş yaptınız ")
+else:
+    print("Kullanıcı adı veya şifre yanlıştır!!!")
+    
+### extra #####
+
+d = {
+ "username" : "alabora33",
+ "password" : "12345"              
+}    
+
+kullaniciadi = input("Kullanıcı adını giriniz:")
+sifre = input("Şifreyi giriniz:")
+
+if(d.get("username")==kullaniciadi and d.get("password")==sifre):
+    print("Başarıyla giriş yaptınız ")
+else:
+    print("Kullanıcı adı veya şifre yanlıştır!!!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
